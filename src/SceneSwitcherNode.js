e5579de8faab3b68fdb7422f455dfb7e237f2983
Péter Gombos <peter@gombos.no>
2019-06-11 20:46:41 +0200

    Initial commit!

---
+++ src/SceneSwitcherNode.js
@@ -0,0 +1,36 @@
+(function(global) {
+  class SceneSwitcherNode extends NIN.Node {
+    constructor(id) {
+      super(id, {
+        inputs: {
+          A: new NIN.TextureInput(),
+          B: new NIN.TextureInput(),
+        },
+        outputs: {
+          render: new NIN.TextureOutput(),
+        }
+      });
+    }
+
+    beforeUpdate() {
+      this.inputs.A.enabled = false;
+      this.inputs.B.enabled = false;
+
+      let selectedScene;
+      if (BEAN < 48 * 4) {
+        selectedScene = this.inputs.A;
+      } else {
+        selectedScene = this.inputs.B;
+      }
+
+      selectedScene.enabled = true;
+      this.selectedScene = selectedScene;
+    }
+
+    render() {
+      this.outputs.render.setValue(this.selectedScene.getValue());
+    }
+  }
+
+  global.SceneSwitcherNode = SceneSwitcherNode;
+})(this);
---
+++ src/spinningCube.js
@@ -0,0 +1,31 @@
+(function(global) {
+  class spinningCube extends NIN.THREENode {
+    constructor(id, options) {
+      super(id, {
+        camera: options.camera,
+        outputs: {
+          render: new NIN.TextureOutput()
+        }
+      });
+
+      this.cube = new THREE.Mesh(new THREE.BoxGeometry(50, 50, 50),
+                                 new THREE.MeshStandardMaterial());
+      this.scene.add(this.cube);
+
+      var light = new THREE.PointLight(0xffffff, 1, 100);
+      light.position.set(50, 50, 50);
+      this.scene.add(light);
+
+      this.camera.position.z = 100;
+    }
+
+    update(frame) {
+      super.update(frame);
+
+      this.cube.rotation.x = Math.sin(frame / 50);
+      this.cube.rotation.y = Math.cos(frame / 50);
+    }
+  }
+
+  global.spinningCube = spinningCube;
+})(this);
