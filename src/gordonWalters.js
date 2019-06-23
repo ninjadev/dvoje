f105fac430238c3661c4c01d42ea14c509112629
Trond Klakken <trond@klakken.com>
2019-06-23 18:00:58 +0200

    Simple working demo, but should rather be plain canvas, rewriting

--- src/gordonWalters.js
+++ src/gordonWalters.js
@@ -1,3 +1,4 @@
+/// <reference path='three.js' />
 (function(global) {
   class gordonWalters extends NIN.THREENode {
     constructor(id, options) {
@@ -8,61 +9,99 @@
         }
       });
 
-      var lineHeight = 4;
-      this.lines = [], this.circles = [];
+      this.lineHeight = 4;
+      this.linesAndCircles = [];
       var screenHeight = 1080;
+      var screenWidth = 1980;
+      this.lineWidth = screenWidth;
 
-      var lineGenerator = function(color, posY) {
-        var line = new THREE.Mesh(new THREE.BoxGeometry(400, lineHeight, lineHeight), new THREE.MeshBasicMaterial({color: color}))
-        line.position.y = posY
-        return line
-      }
-      var circleGenerator = function(color, posY) {
+      var color = 0;
+      var colors = [0xcccbc4, 0x232321]
+      for(var posY = -screenHeight/2; posY < screenHeight/2; posY += this.lineHeight) {
+        var xOffset = (-this.lineWidth/2 * Math.random()) + this.lineWidth/4;
+        var leftLine = this.lineGenerator(colors[color % 2], posY, -this.lineWidth/2 + xOffset);
+        var rightLine = this.lineGenerator(colors[color % 2], posY, 13 + (this.lineWidth/2) + xOffset);
+        var centerLine = this.lineGenerator(colors[(color+1) % 2], posY+2, 0, this.lineHeight+2);
+        centerLine.position.z = -10;
+
+        var leftCircle = this.circleGenerator(colors[color % 2], posY + this.lineHeight/2, 0 + xOffset);
+        var rightCircle = this.circleGenerator(colors[color % 2], posY + this.lineHeight/2, 12 + xOffset);
+
         var direction = Math.random() < 0.5 ? -1 : 1
-        var circle = new THREE.Mesh(new THREE.SphereGeometry(lineHeight, 16, 16), new THREE.MeshBasicMaterial({color: color}))
-        circle.position.y = posY+ (direction * (lineHeight/2))
-        circle.position.x = Math.random()*200*direction;
-        return {obj: circle, direction: direction, acc: (Math.random() * direction)}
+
+        this.linesAndCircles.push({
+          circles: [leftCircle, rightCircle],
+          lines: [centerLine, leftLine, rightLine],
+          direction: direction,
+          acc: (Math.random() * direction)
+        });
+
+        color++;
       }
 
-      for(var posY = -200; posY < -200+screenHeight; posY += 2*lineHeight+1) {
-        this.lines.push(lineGenerator(0xffffff, posY));
-        this.circles.push(circleGenerator(0xffffff, posY+lineHeight));
-      }
 
-      for(var line of this.lines) {
-        this.scene.add(line)
+      for(var obj of this.linesAndCircles) {
+        for(var line of obj.lines) {
+          this.scene.add(line)
+        }
+        for(var circle of obj.circles) {
+          this.scene.add(circle)
+        }
       }
-      for(var circle of this.circles) {
-        this.scene.add(circle.obj)
-      }
-
+      
+      var plane = new THREE.Mesh(new THREE.PlaneGeometry(screenHeight, screenWidth),  new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+      }))
+      this.scene.add(plane)
       var light = new THREE.PointLight(0x00, 1, 100);
       light.position.set(50, 50, 50);
 
-      //this.scene.add(light);
-
-      this.camera.position.z = 100;
+      this.scene.add(light);
+      this.camera.position.z = -100;
     }
 
     update(frame) {
       super.update(frame);
-      
-      for(var line of this.lines) {
-        line.position.y -= 0.3;
+
+      for(var obj of this.linesAndCircles) {
+
+        for(var line of obj.lines) {
+          line.position.y -= 0.3;
+          line.position.x += obj.acc;
+        }
+        for(var circle of obj.circles) {
+          circle.position.y -= 0.3;
+          circle.position.x += obj.acc;
+          /*
+          if (circle.position.x > 200) {
+            circle.position.x = -200;
+          }
+          if (circle.position.x < -200) {
+            circle.position.x = 200;
+          }
+          */
+        }
+        
       }
-      for(var circ of this.circles) {
-        var circle = circ.obj;
-        circle.position.y -= 0.3;
-        circle.position.x += circ.acc;
-        if (circle.position.x > 200) {
-          circle.position.x = -200;
-        }
-        if (circle.position.x < -200) {
-          circle.position.x = 200;
-        }
-      } 
-      this.camera.position.z = 100 + (20 * Math.cos(frame / 80));
+      
+      
+      this.camera.position.z = 300 + (30 * Math.cos(frame / 80));
+      //this.camera.rotation.x = 30 * Math.cos(frame / 80) * Math.PI / 180
+    }
+
+
+    lineGenerator(color, posY, posX, lineHeight2 = this.lineHeight) {
+      var line = new THREE.Mesh(new THREE.BoxGeometry(this.lineWidth, lineHeight2, lineHeight2), new THREE.MeshBasicMaterial({color: color}))
+      line.position.y = posY
+      line.position.x = posX
+      return line
+    }
+
+    circleGenerator(color, posY, posX) {
+      var circle = new THREE.Mesh(new THREE.SphereGeometry(this.lineHeight, 16, 16), new THREE.MeshBasicMaterial({color: color}))
+      circle.position.y = posY;
+      circle.position.x = posX;
+      return circle
     }
   }
 
