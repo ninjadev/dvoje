cef129731481093d74f39adba72ed56527cb28c5
Simon Randby <simon.randby@gmail.com>
2019-07-13 11:09:27 +0200

    Clear up code a bit

--- src/greetsBackgroundCanvas.js
+++ src/greetsBackgroundCanvas.js
@@ -29,72 +29,26 @@
 
     update(frame) {
       super.update(frame);
-      this.ctx.save();
+      if(BEAN % 4 === 0){
+        this.ctx.save();
 
-      var randomColor = `#${Math.floor(this.random() * 0x1000000).toString(16).padStart(6, '0')}`;
+        var randomColor = `#${Math.floor(this.random() * 0x1000000).toString(16).padStart(6, '0')}`;
+        // Use random color for drawing>
+        this.ctx.fillStyle = randomColor;
+        this.ctx.beginPath();
+        // Random x/y centerpoint that is within scene:
+        var x = Math.floor(this.random() * 900);
+        var y = Math.floor(this.random() * 900);
+        // Random size of circle, but within reasonable bounds:
+        var maxCircleRadius = this.canvas.width / 5;
+        var minCircleRadius = this.canvas.width / 15;
+        var radius = Math.floor(this.random() * (maxCircleRadius - minCircleRadius) + minCircleRadius);
+        // Draw circle based on config above
+        this.ctx.arc(x, y, radius, 0, Math.PI * 2);
+        this.ctx.fill();
 
-      // Use random color for drawing>
-      this.ctx.fillStyle = randomColor;
-      this.ctx.beginPath();
-      // Random x/y centerpoint that is within scene:
-      var x = Math.floor(this.random() * 900);
-      var y = Math.floor(this.random() * 900);
-      // Random size of circle, but within reasonable bounds:
-      var maxCircleRadius = this.canvas.width / 5;
-      var minCircleRadius = this.canvas.width / 15;
-      var radius = Math.floor(this.random() * (maxCircleRadius - minCircleRadius) + minCircleRadius);
-      this.ctx.arc(x, y, radius, 0, Math.PI * 2);
-      this.ctx.fill();
-
-      // this.ctx.fillRect(
-      //   x, // x
-      //   y, // y
-      //   Math.random() * size, // width
-      //   Math.random() * size // height
-      // );
-
-      // section of nope:
-      // this.ctx.beginPath();
-      // this.ctx.moveTo(400, 50);
-      // this.ctx.lineTo(1024, 0);
-      // this.ctx.lineTo(5, 800);
-      // this.ctx.moveTo(0, 0);
-      // this.ctx.lineTo(15, 15);
-      // this.ctx.stroke();
-
-      // var canvas_size = 1024;
-      // var size = 800;
-      // var width_small = 15;
-      // var width_large = 50;
-      // this.ctx.fillStyle = '#6c79ae';
-      // this.ctx.fillRect((canvas_size - size) / 2,
-      //                   (canvas_size - size) / 2,
-      //                   size,
-      //                   size);
-      // this.ctx.clearRect((canvas_size - size) / 2 + width_small,
-      //                     (canvas_size - size) / 2 + width_small,
-      //                     size - 2 * width_small,
-      //                     size - 2 * width_small);
-
-      // this.ctx.fillRect((canvas_size - size) / 2 + 2 * width_small,
-      //                   (canvas_size - size) / 2 + 2 * width_small,
-      //                   size - 4 * width_small,
-      //                   size - 4 * width_small);
-      // this.ctx.clearRect((canvas_size - size) / 2 + width_small + width_large,
-      //                     (canvas_size - size) / 2 + width_small + width_large,
-      //                     size - 2 * width_small - 2 * width_large,
-      //                     size - 2 * width_small - 2 * width_large);
-
-      // this.ctx.fillRect((canvas_size - size) / 2 + 2 * width_small + width_large,
-      //                   (canvas_size - size) / 2 + 2 * width_small + width_large,
-      //                   size - 4 * width_small - 2 * width_large,
-      //                   size - 4 * width_small - 2 * width_large);
-      // this.ctx.clearRect((canvas_size - size) / 2 + 3 * width_small + width_large,
-      //                     (canvas_size - size) / 2 + 3 * width_small + width_large,
-      //                     size - 6 * width_small - 2 * width_large,
-      //                     size - 6 * width_small - 2 * width_large);
-
-      this.ctx.restore();
+        this.ctx.restore();
+      }
     }
 
     render() {
